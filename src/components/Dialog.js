a0f5eb37faf11c59a12b6f368a35bd9fc6c21fbf
zzz-unsu <peulri1379@gmail.com>
2022-11-11 22:36:51 +0900

    웹페이지 전환 대체 Dialog

---
+++ src/components/Dialog.js
@@ -0,0 +1,57 @@
+import * as React from 'react';
+import { Paper, Grid, Box } from '@mui/material';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import CloseIcon from '@mui/icons-material/Close';
+
+const FullDialog = ({title, contents}) => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        Open full-screen dialog
+      </Button>
+      <Dialog
+        fullScreen
+        open={open}
+        onClose={handleClose}
+        sx={{
+            padding: '24px'
+        }}
+      >
+        <AppBar sx={{ position: 'relative' }}>
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              onClick={handleClose}
+              aria-label="close"
+            >
+              <CloseIcon />
+            </IconButton>
+            <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">{title}</Typography>
+            <Button autoFocus color="inherit" onClick={handleClose}>Close</Button>
+          </Toolbar>
+        </AppBar>
+        <Box sx={{padding: "24px"}}>
+            {contents}
+        </Box>
+      </Dialog>
+    </div>
+  );
+}
+
+export default FullDialog;
