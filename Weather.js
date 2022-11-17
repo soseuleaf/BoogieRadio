1f40b9505fb8de42e1311241079345b2a65110ab
Soomin Kim <soomin317@naver.com>
2022-11-17 23:44:39 +0900

    Update Weather.js

--- Weather.js
+++ Weather.js
@@ -1,45 +1,41 @@
-//import React from 'react';
-function GetWeather() {
-    //API 이용
-    const API_KEY = "0c90fa9985e8aedff752ac5f1dc92024";
-    const city = "seoul";
-    
-    //const [temp, setTemp]=useState("");
-    //const [icon, setIcon]=useState("");
-    //const [query, setQuery]=useState("");
-    
-    //DOM객체들
-    const weatherInfo = document.querySelector('.weatherInfo');
-    const weatherIconImg = document.querySelector('.weatherIcon');
-    //날씨 api를 통해 날씨에 관련된 정보들을 받아오기: 도시: 서울, 단위: 섭씨(도)
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
-    .then(res => res.json())
-    .then(function(json) {
-        
-        //온도, 위치, 날씨묘사, 날씨아이콘을 받는다. 
-        
-        const temperature = json.main[0].temp;
-        const weatherDescription = json.weather[0].description;
-        //description에 따라 아이콘 따로 띄우기
-        const weatherIcon = json.weather[0].icon;
-        const weatherIconAdrs = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
-        
+import React, { useState } from "react";
+import axios from "axios";
 
-        //받아온 정보들을 표현한다. 원래는 온도 옆에 도시이름도 있음(@${city})
-        weatherInfo.innerText = `${temperature} °C`;
-        weatherIconImg.setAttribute('src', weatherIconAdrs);
-    })
+const api = {
+    key: "ebf763f3403fce3adecc129c284dbcf4",
+    base: "https://api.openweathermap.org/data/2.5/",
+};
+
+function Weather({setCold}) {
+    const city = "Seoul";
+    const url = `${api.base}weather?q=${city}&appid=${api.key}`;
+    const [weather, setWeather] = useState("");
     
-    .catch((error) => console.log("error:", error)); 
-    
+
+  // 날씨 가져오기
+    axios.get(url).then((responseData) => {
+        const data = responseData.data;
+        setWeather({
+        icon: data.weather[0].icon,
+        temperature: data.main.temp,
+        main: data.weather[0].main,
+        loading: false,
+        });
+    });
+
+    setCold(weather.temperature < 290 ? true : false);
+    const iconurl=`http://openweathermap.org/img/wn/${weather.icon}@2x.png`;
+
     return (
-        <>
-        <div style={{width:'100px', height:'100px', backgroundcolor:'pink'}}>
-        <div className="temp">{temperature}</div>
-        <img src='http://openweathermap.org/img/wn/{weatherIconImg}@2x.png'></img>
+    <>
+        
+        <div className="weatherBox">
+            <div id="Temperature" style={{color:"white", fontSize:"10px"}}>{(weather.temperature -273.15).toFixed(2)}℃</div>
+            <div id="WeatherDiv" style={{color:"white", fontSize:"10px"}}>{weather.main}</div>
+            <img src={iconurl} style={{width:"20px", height:"20px"}}alt=" "/>
+            
         </div>
-        </>
-    )
-    
+    </>
+    );
 }
-export default GetWeather;
+export default Weather;
