448442e12d0821dbcce3198fdece5f79a2bfbb74
Soomin Kim <soomin317@naver.com>
2022-11-18 14:02:44 +0900

    Traffic 추가
    kakaomap api 이용

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./App.css";
 import Weather from "./Weather";
+import Traffic from "./Traffic";
 
 
 function App() {
     return (
+        <Traffic/>
         <Weather/>
+
     );
 }
 
